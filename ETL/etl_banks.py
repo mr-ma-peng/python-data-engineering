e782b28500bdf57a6340040bc448c5217fc62f2d
Peng Ma <peng.ma@thoughtworks.com>
2024-01-05 14:00:01 +0800

    [Peng Ma][N/A] feat: load data & query data

--- ETL/etl_banks.py
+++ ETL/etl_banks.py
@@ -11,6 +11,13 @@ url = 'https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/
 table_attributes = ['Name', 'MC_USD_Billion']
 exchange_rate_csv_pate = './banks_latest_data/exchange_rate.csv'
 output_path = './banks_latest_data/output_result.csv'
+data_base_name = 'Banks.db'
+table_name = 'Largest_banks'
+
+count_query_statement = 'SELECT * FROM Largest_banks'
+avg_query_statement = 'SELECT AVG(MC_GBP_Billion) FROM Largest_banks'
+name_query_statement = 'SELECT Name from Largest_banks LIMIT 5'
+
 
 
 def log_progress(message):
@@ -67,16 +74,21 @@ def transform(df, csv_path):
 def load_to_csv(df, output_path):
     """ This function saves the final data frame as a CSV file in
     the provided path. Function returns nothing."""
+    df.to_csv(output_path)
 
 
 def load_to_db(df, sql_connection, table_name):
     """ This function saves the final data frame to a database
     table with the provided name. Function returns nothing."""
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
 
 
 def run_query(query_statement, sql_connection):
     """ This function runs the query on the database table and
     prints the output on the terminal. Function returns nothing. """
+    print(query_statement)
+    query_result = pd.read_sql(query_statement, sql_connection)
+    print(query_result)
 
 
 ''' Here, you define the required entities and call the relevant
@@ -90,12 +102,14 @@ log_progress("Data extraction complete. Initiating Transformation process")
 df = transform(df, exchange_rate_csv_pate)
 log_progress("Data transformation complete. Initiating Loading process")
 load_to_csv(df, output_path)
-# log_progress("Data saved to CSV file")
-# sql_connection = sqlite3.connect("")
-# log_progress("SQL Connection initiated")
-# load_to_db(df, sql_connection, table_name)
-# log_progress("Data loaded to Database as a table, Executing queries")
-# run_query("SELECT * FROM", sql_connection)
-# log_progress("Process Complete")
-# sql_connection.commit()
-# log_progress("Server Connection closed")
+log_progress("Data saved to CSV file")
+sql_connection = sqlite3.connect(data_base_name)
+log_progress("SQL Connection initiated")
+load_to_db(df, sql_connection, table_name)
+log_progress("Data loaded to Database as a table, Executing queries")
+run_query(count_query_statement, sql_connection)
+run_query(avg_query_statement, sql_connection)
+run_query(name_query_statement, sql_connection)
+log_progress("Process Complete")
+sql_connection.commit()
+log_progress("Server Connection closed")
